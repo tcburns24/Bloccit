9600620ba74ad2cf58b930fadefb10954b9ecd92
Thomas Burns <thomasburns@Thomass-MBP.frontier.com>
2016-08-18 14:10:18 -0400

    Completed Assignment-30

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,10 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "Assignment 30 Post", body: "assignment 30 post body")
+puts "#{Post.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
