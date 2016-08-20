20d44ee970fe022a4f015b39704e31249d93132f
Thomas Burns <thomasburns@Thomass-MBP.frontier.com>
2016-08-20 15:50:55 -0400

    Completed Checkpoint-34

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
-
-  get 'welcome/contact'
 
 end
