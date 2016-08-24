6817ebdd2ce7e700cfa96f756d38e3103916eaa2
Thomas Burns <thomasburns@Thomass-MacBook-Pro.local>
2016-08-24 15:51:18 -0700

    Update Master with revised user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts
-  before_save { self.email = email.downcase if email.present? }
+
+  before_save { self.email = email.downcase }
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, unless :password_digest
+  validates :password, presence: true, length: { minimum: 6 }, if "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
             presence: true,
