4804d6165177674faad6f85449f75cf270047416
Thomas Burns <thomasburns@Thomass-MacBook-Pro.local>
2016-08-29 17:53:16 -0700

    2nd time Assignment-42

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
 
   resources :topics do
     resources :posts, except: [:index]
+    resources :comments, only: [:create, :destroy]
   end
-
-  resources :comments, only: [:create, :destroy]
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
