c610ffd20d1a9e0d4ba29cfcf16faec8bd1e55f8
Thomas Burns <thomasburns@Thomass-MacBook-Pro.local>
2016-09-01 14:34:40 -0700

    3rd Attempt Assignment-49

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,29 +3,51 @@ class Api::V1::TopicsController < Api::V1::BaseController
   before_action :authenticate_user, except: [:index, :show]
   before_action :authorize_user, except: [:index, :show]
 
-  def create
-    post = Post.new(post_params)
-
-    if post.valid?
-      post.save!
-      render json post, status: 201
-    else
-      render json: {error: "Post is invalid", status: 400}, status: 400
-    end
+  def index
+    posts = Post.all
+    render json: posts.to_json, status: 200
   end
 
+  def show
+    post = Post.find(params[:id])
+    render json: post.to_json(include: [:comments, :votes, :favorites]), status: 200
+  end
 
   def update
     post = Post.find(params[:id])
 
     if post.update_attributes(post_params)
-      render json: post, status: 200
+      render json: post.to_json, status: 200
     else
       render json: {error: "Post update failed", status: 400}, status: 400
     end
   end
 
-
   def destroy
+    post = Post.find(params[:id])
+
+    if post.destroy
+      render json: {message: "Post destroyed", status: 200}, status: 200
+    else
+      render json: {error: "Post destroy failed", status: 400}, status: 400
+    end
+  end
+
+  def create
+    topic = Topic.find(params[:topic_id])
+    post = topic.posts.build(post_params)
+    post.user = @current_user
+
+    if post.save
+      render json: post.to_json, status: 200
+    else
+      render json: {error: "Post create failed", status: 400}, status: 400
+    end
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
