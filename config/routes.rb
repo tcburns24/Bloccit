2aabe9a9bde647b02d2aec9a96c91367154483b4
Thomas Burns <tcburns24@gmail.com>
2017-09-01 14:37:27 -0700

    bloccit updates 9/1/17

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Rails.application.routes.draw do
 
   resources :labels, only: [:show]
@@ -24,11 +25,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, except: [:edit, :new] do
-        resources :posts, only: [:create]
-      end
-      resources :posts, only: [:index, :show, :update, :destroy]
-      resources :comments, only: [:index, :show]
+      resources :topics, except: [:edit, :new]
     end
   end
 end
